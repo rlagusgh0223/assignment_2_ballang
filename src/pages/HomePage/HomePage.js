74d1e745378be7c66e3c7eb77990077ff4b58d87
rlagusgh0223 <hopanman11@naver.com>
2024-02-04 13:49:04 +0900

    .

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -5,18 +5,21 @@ function HomePage() {
   const [data, setData] = useState({ product: [] });
 
   console.log("useState", data);
-  // 컴포넌트가 마운트될 때 데이터를 가져옴
-  useEffect(() => {
-    // API 엔드포인트
-    const url =
-      "https://port-0-ballang-server-qrd2als49b8m4.sel5.cloudtype.app/products";
 
-    // fetch를 사용하여 데이터 가져오기
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((error) => console.error("Error:", error));
-  });
+  useEffect(() => {});
+
+  // // 컴포넌트가 마운트될 때 데이터를 가져옴
+  // useEffect(() => {
+  //   // API 엔드포인트
+  //   const url =
+
+  //   // fetch를 사용하여 데이터 가져오기
+  //   fetch(url)
+  //     .then((response) => response.json())
+  //     .then((data) => console.log(data))
+  //     .catch((error) => console.error("Error:", error));
+  // });
+
   console.log("after", data);
   return <div>HomePage</div>;
 }
